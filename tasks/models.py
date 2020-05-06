6a587cb61008f97df7e6839d852f9d67e359c466
Osam Kyemenu-Sarsah <papasarsah@gmail.com>
2020-05-06 19:44:46 +0000

    added creator to model Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 class Task(models.Model):
 
     title = models.CharField(max_length=250)
     complete = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
+    creator = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('tasklistcreate')
+        return reverse('tasklistcreate', args=[str(self.id)])
