6b8d8f6d6f9ba94923d63fc0b852c1c1d1209157
Osam Kyemenu-Sarsah <papasarsah@gmail.com>
2020-05-08 10:54:37 +0000

    added 404 template and set debug=False

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jwv86dvh&hx4-4aw^tw3f38(n3)j*+-%#or2k4@lvz+fqpv$=q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
